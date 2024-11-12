95a1f4868c50a2f5833bdd7216a54ec267acc451
Yasmin.Gudha <yasmin.gudha@develeap.com>
2024-11-12 08:53:11 +0200

    added envs

--- rest-microservice/app/app.py
+++ rest-microservice/app/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 import boto3
 from botocore.exceptions import ClientError
+import os
 
 app = Flask(__name__)
 
-secret_name = "yasmin_checkpoint_home_exam_token"
-region_name = "us-east-1"
-sqs_queue_url = "https://sqs.us-east-1.amazonaws.com/YOUR_ACCOUNT_ID/YOUR_QUEUE_NAME"
+secret_name = os.environ.get("SECRET_NAME", "default_secret_name")
+region_name = os.environ.get("REGION_NAME", "us-east-1")
+sqs_queue_url = os.environ.get("SQS_QUEUE_URL", "default_sqs_queue_url")
 
 session = boto3.session.Session()
 secret_client = session.client(
